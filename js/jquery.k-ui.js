16f3e5e0006c4c43056d93a4ad0ad95dffcd90ea
Nikolai Khilkovsky <khilkovn@gmail.com>
2014-10-14 02:16:06 +0300

    DEV-2169 - main structure and header

---
+++ js/jquery.k-ui.js
@@ -0,0 +1,4 @@
+/*
+ * IE8 HTML5 tags support
+ */
+if(document.all && !document.addEventListener){document.createElement('address');document.createElement('article');document.createElement('aside');document.createElement('details');document.createElement('figcaption');document.createElement('figure');document.createElement('footer');document.createElement('header');document.createElement('hgroup');document.createElement('main');document.createElement('nav');document.createElement('section');document.createElement('summary');document.createElement('audio');document.createElement('canvas');document.createElement('video');document.createElement('time');document.createElement('ins');}
